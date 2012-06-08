3e38253521fc47c3dfe5f906ac12052eb692fe89
Dane Summers <dsummersl@yahoo.com>
2012-06-07 23:28:18 -0400

    upgraded to jqtouch-1.0-b4 - fixed jspec tests

--- www/test/calendrical/spec/unit/spec.js
+++ www/test/calendrical/spec/unit/spec.js
@@ -1,28 +1,28 @@
 
 describe 'GUI Things: '
 	before_each
-		appendTo = $(fixture('appendable'))
-		len = calendars.length;
+		appendTo = $(fixture('appendable.html'))
+		len = Object.keys(implementations).length;
 	end
 
   describe 'the makeList function'
 		it 'show all calendars when none are excluded'
 			makeList(appendTo,'named');
 			appendTo.should.have_tag 'ul'
-			for (var i=0; i<len; i++) {
-				appendTo.find('option[value='+ calendars[i] +']').should.have_tag "<option>"
+      for (var key in implementations) {
+				appendTo.find('option[value='+ key +']').should.have_tag "<option>"
 			}
     end
 
 		it 'show all but excluded calendars'
 			makeList(appendTo,'named','julian');
 			appendTo.should.have_tag 'ul'
-			for (var i=0; i<len; i++) {
-				if (calendars[i] != 'julian') {
-					appendTo.find('option[value='+ calendars[i] +']').should.have_tag "<option>"
+      for (var key in implementations) {
+				if (key != 'julian') {
+					appendTo.find('option[value='+ key +']').should.have_tag "<option>"
 				}
 				else {
-					appendTo.find('option[value='+ calendars[i] +']').length.should.eql 0
+					appendTo.find('option[value='+ key +']').length.should.eql 0
 				}
 			}
 		end
